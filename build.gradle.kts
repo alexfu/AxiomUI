43989297bcbb9927a388f971b1100d7a4dfe30b3
Alex Fu <alexfu@fastmail.com>
2026-02-22 16:49:45 -0500

    add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.dokka)
 }
 
 allprojects {
@@ -26,4 +27,10 @@ allprojects {
             }
         }
     }
+}
+
+dependencies {
+    dokka(project(":state"))
+    dokka(project(":command"))
+    dokka(project(":viewmodel"))
 }
